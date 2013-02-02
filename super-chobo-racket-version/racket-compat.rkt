9420d36e0f094043c5a7101912fe37c18c7876f2
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-02-02 12:33:54 -0500

    More idiomatic racket code.
    Better definitions for `remp` and `datum->string`.

--- super-chobo-racket-version/racket-compat.rkt
+++ super-chobo-racket-version/racket-compat.rkt
@@ -2,16 +2,9 @@
 
 (provide remp my-sort datum->string)
 
-(define (remp p ls)
-  (cond
-    ((null? ls) '())
-    ((p (car ls)) (remp p (cdr ls)))
-    (else (cons (car ls) (remp p (cdr ls))))))
+(define (remp p ls) (filter (compose not p) ls))
 
 (define (my-sort comp ls)
   (sort ls comp))
 
-(define datum->string
-  (lambda (x)
-    (with-output-to-string
-      (lambda () (display x)))))
+(define datum->string ~a)
